dc5bd870849b8cc292c1167923b61820f883ac37
Aleksey <aleksey.kurkov@gmail.com>
2015-03-24 01:06:11 +0200

    Initial commit

---
+++ src/DiscJockey.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Aleksey Kurkov on 24.03.15.
+ */
+
+public class DiscJockey {
+
+  SongComponent songList;
+
+  // newSongList contains every Song, SongGroup,
+  // and any Songs saved in SongGroups
+
+  public DiscJockey(SongComponent newSongList) {
+
+    songList = newSongList;
+
+  }
+
+  // Calls the displaySongInfo() on every Song
+  // or SongGroup stored in songList
+
+  public void getSongList() {
+
+    songList.displaySongInfo();
+
+  }
+
+}
---
+++ src/Song.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Aleksey Kurkov on 24.03.15.
+ */
+
+public class Song extends SongComponent {
+
+  String songName;
+  String bandName;
+  int releaseYear;
+
+  public Song(String newSongName, String newBandName, int newReleaseYear) {
+
+    songName = newSongName;
+    bandName = newBandName;
+    releaseYear = newReleaseYear;
+  }
+
+  @Override
+  public String getSongName() {
+
+    return songName;
+
+  }
+
+  @Override
+  public String getBandName() {
+
+    return bandName;
+
+  }
+
+  @Override
+  public int getReleaseYear() {
+
+    return releaseYear;
+
+  }
+
+  @Override
+  public void displaySongInfo() {
+
+    System.out.println(getSongName() + " was recorded by " + getBandName() + " in " + getReleaseYear());
+
+  }
+}
